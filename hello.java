484bae74354eb344976ab48aa906db2286356f6b
geonbeom <goyard777@naver.com>
2021-04-19 12:12:35 +0900

    commit

---
+++ hello.java
@@ -0,0 +1,12 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        for(int i=2; i<9; i++){
+            for(int e=3; e<10; --e){
+                System.out.println(i*e);
+            }
+        }
+
+    }
+    
+}
